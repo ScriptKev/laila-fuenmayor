7ce80d734ab9a854cd4f4ebf10587fb26415d782
ScriptKev <script.kev@gmail.com>
2021-06-22 07:40:53 -0400

    agregando salto de linea para mobiles

--- components/pages/courses/CTA/index.tsx
+++ components/pages/courses/CTA/index.tsx
@@ -3,7 +3,7 @@ export const CTA = () => {
     <section className='w-full h-full bg-CTABg bg-no-repeat bg-center bg-cover py-20'>
       <div className='flex justify-center items-center md:mx-0 mx-4'>
         <div className='max-w-xl 2xl:max-w-3xl h-full py-5 px-4 bg-white bg-opacity-70 text-center rounded-xl'>
-          <h4 className='font-hottemp text-6xl text-primary-300 2xl:text-8xl'>Enámorate de tu sello</h4>
+          <h4 className='font-hottemp text-6xl text-primary-300 2xl:text-8xl'>Enámorate <br className='ml:hidden' /> de tu sello</h4>
           <br />
           <p className='font-semibold text-gray-800 2xl:text-xl'>
             Únete a este programa de 6 semanas en vivo en el que te guiaré por el paso a paso que necesitas para prepararte, descubrir, amar e
